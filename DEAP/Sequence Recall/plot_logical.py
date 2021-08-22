b92f3dc15c85adb8e97be8bb4414ffc75b47ebf5
Mihyar Al Masalma <eng.mihyear@gmail.com>
2021-08-21 22:39:30 -0300

    Add Logical Reports

---
+++ DEAP/Sequence Recall/plot_logical.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+import pickle
+import os
+
+local_dir = os.path.dirname(__file__)
+axis_x = list(range(0, 251))
+ngen = len(axis_x) - 1
+y_label = "Success Percentage"
+x_label = "Training Generations"
+
+# 4 Deep Report
+for i in range(1,21):
+    path = os.path.join(local_dir, '4-deep-report/4-progress_report_logic' + str(i))
+    with open(path, 'rb') as f:
+        info = pickle.load(f)
+    plt.plot(axis_x, info, linewidth=1)
+
+plt.ylabel(y_label)
+plt.xlabel(x_label)
+plt.title(str(ngen) + "-Gen 4-deep Report using logical operators")
+plt.show()
+
+# 5 Deep Report
+for i in range(1,21):
+    path = os.path.join(local_dir, '5-deep-report/5-progress_report_logic' + str(i))
+    with open(path, 'rb') as f:
+        info = pickle.load(f)
+    plt.plot(axis_x, info, linewidth=1)
+
+plt.ylabel(y_label)
+plt.xlabel(x_label)
+plt.title(str(ngen) + "-Gen 5-deep Report using logical operators")
+plt.show()
+
+# 6 Deep Report
+for i in range(1,21):
+    path = os.path.join(local_dir, '6-deep-report/6-progress_report_logic' + str(i))
+    with open(path, 'rb') as f:
+        info = pickle.load(f)
+    plt.plot(axis_x, info, linewidth=1)
+
+plt.ylabel(y_label)
+plt.xlabel(x_label)
+plt.title(str(ngen) + "-Gen 6-deep Report using logical operators")
+plt.show()
+
+# 15 Deep Report
+for i in range(1,21):
+    path = os.path.join(local_dir, '15-deep-report/15-progress_report_logic' + str(i))
+    with open(path, 'rb') as f:
+        info = pickle.load(f)
+    plt.plot(axis_x, info, linewidth=1)
+
+plt.ylabel(y_label)
+plt.xlabel(x_label)
+plt.title(str(ngen) + "-Gen 15-deep Report using logical operators")
+plt.show()
+
+# 21 Deep Report
+for i in range(1,21):
+    path = os.path.join(local_dir, '21-deep-report/21-progress_report_logic' + str(i))
+    with open(path, 'rb') as f:
+        info = pickle.load(f)
+    plt.plot(axis_x, info, linewidth=1)
+
+plt.ylabel(y_label)
+plt.xlabel(x_label)
+plt.title(str(ngen) + "-Gen 21-deep Report using logical operators")
+plt.show()
