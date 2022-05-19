35ec921e613c61a3bc8f0f182b526724ee5927c5
Mihyar Al Masalma <eng.mihyear@gmail.com>
2022-05-19 00:38:25 -0300

    Refactor

--- Plotting/sequence_recall.py
+++ Plotting/sequence_recall.py
@@ -4,8 +4,8 @@ import os
 import csv
 
 local_dir = os.path.dirname(__file__)
-axis_x_250 = list(range(0, 251))
-axis_x_500 = list(range(0, 500))
+axis_x_250 = list(range(251))
+axis_x_500 = list(range(500))
 y_label = "Success Percentage"
 x_label = "Training Generations"
 
@@ -15,10 +15,10 @@ x_label = "Training Generations"
 
 # 4 Deep Report
 plt.figure(1)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/4-deep-report/4-progress_report' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/4_report_std')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -27,10 +27,10 @@ plt.savefig("../Plotting/Sequence Recall/DEAP_4_Standard.png", bbox_inches='tigh
 
 # 5 Deep Report
 plt.figure(2)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/5-deep-report/5-progress_report' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/5_report_std')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -39,10 +39,10 @@ plt.savefig("../Plotting/Sequence Recall/DEAP_5_Standard.png", bbox_inches='tigh
 
 # 6 Deep Report
 plt.figure(3)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/6-deep-report/6-progress_report' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/6_report_std')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -51,10 +51,10 @@ plt.savefig("../Plotting/Sequence Recall/DEAP_6_Standard.png", bbox_inches='tigh
 
 # 15 Deep Report
 plt.figure(4)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/15-deep-report/15-progress_report' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/15_report_std')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -63,10 +63,10 @@ plt.savefig("../Plotting/Sequence Recall/DEAP_15_Standard.png", bbox_inches='tig
 
 # 21 Deep Report
 plt.figure(5)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/21-deep-report/21-progress_report' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/21_report_std')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -80,10 +80,10 @@ plt.show()
 
 # 4 Deep Report
 plt.figure(6)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/4-deep-report/4-progress_report_logic' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/4_report_log')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -92,10 +92,10 @@ plt.savefig("../Plotting/Sequence Recall/DEAP_4_Logic.png", bbox_inches='tight')
 
 # 5 Deep Report
 plt.figure(7)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/5-deep-report/5-progress_report_logic' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/5_report_log')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -104,10 +104,10 @@ plt.savefig("../Plotting/Sequence Recall/DEAP_5_Logic.png", bbox_inches='tight')
 
 # 6 Deep Report
 plt.figure(8)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/6-deep-report/6-progress_report_logic' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/6_report_log')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -116,10 +116,10 @@ plt.savefig("../Plotting/Sequence Recall/DEAP_6_Logic.png", bbox_inches='tight')
 
 # 15 Deep Report
 plt.figure(9)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/15-deep-report/15-progress_report_logic' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/15_report_log')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -128,10 +128,10 @@ plt.savefig("../Plotting/Sequence Recall/DEAP_15_Logic.png", bbox_inches='tight'
 
 # 21 Deep Report
 plt.figure(10)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/21-deep-report/21-progress_report_logic' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/21_report_log')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -145,10 +145,10 @@ plt.show()
 
 # 4 Deep Report
 plt.figure(11)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/4-deep-report/4-progress_report_mul' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/4_report_mul')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -157,10 +157,10 @@ plt.savefig("../Plotting/Sequence Recall/DEAP_4_multiplication.png", bbox_inches
 
 # 5 Deep Report
 plt.figure(12)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/5-deep-report/5-progress_report_mul' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/5_report_mul')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -173,10 +173,10 @@ plt.show()
 '''
 # 4 Deep Report
 plt.figure(13)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/4-deep-report/4-progress_report_mod' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/4_report_mod')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -185,10 +185,10 @@ plt.savefig("../Plotting/Sequence Recall/DEAP_4_mod.png", bbox_inches='tight')
 
 # 5 Deep Report
 plt.figure(14)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/5-deep-report/5-progress_report_mod' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/5_report_mod')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -197,10 +197,10 @@ plt.savefig("../Plotting/Sequence Recall/DEAP_5_mod.png", bbox_inches='tight')
 
 # 6 Deep Report
 plt.figure(15)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/6-deep-report/6-progress_report_mod' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/6_report_mod')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -209,10 +209,10 @@ plt.savefig("../Plotting/Sequence Recall/DEAP_6_mod.png", bbox_inches='tight')
 
 # 15 Deep Report
 plt.figure(16)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/15-deep-report/15-progress_report_mod' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/15_report_mod')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -221,10 +221,10 @@ plt.savefig("../Plotting/Sequence Recall/DEAP_15_mod.png", bbox_inches='tight')
 
 # 21 Deep Report
 plt.figure(17)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../DEAP/Sequence Recall/21-deep-report/21-progress_report_mod' + str(i))
-    with open(path, 'rb') as f:
-        info = pickle.load(f)
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/21_report_mod')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
     plt.plot(axis_x_250, info, linewidth=1)
 
 plt.ylabel(y_label)
@@ -239,16 +239,12 @@ plt.show()
 
 # 4-deep Report
 plt.figure(18)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../NEAT/Sequence Recall/4-deep-report/4-progress_report' + str(i) + '.csv')
-    info = []
-    with open(path, newline='\n') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            fitness = row[0].split()
-            info.append(float(fitness[0]))
-            axis_x = list(range(0, len(info)))
-    plt.plot(axis_x, info, linewidth=1)
+path = os.path.join(local_dir, '../NEAT/Sequence Recall/reports/4_report_std')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
+    info.extend([info[-1] for _ in range(500 - len(info))])
+    plt.plot(axis_x_500, info, linewidth=1)
 
 plt.ylabel(y_label)
 plt.xlabel(x_label)
@@ -258,16 +254,12 @@ plt.savefig("../Plotting/Sequence Recall/NEAT_4_Standard.png", bbox_inches='tigh
 
 # 5-deep Report
 plt.figure(19)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../NEAT/Sequence Recall/5-deep-report/5-progress_report' + str(i) + '.csv')
-    info = []
-    with open(path, newline='\n') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            fitness = row[0].split()
-            info.append(float(fitness[0]))
-            axis_x = list(range(0, len(info)))
-    plt.plot(axis_x, info, linewidth=1)
+path = os.path.join(local_dir, '../NEAT/Sequence Recall/reports/5_report_std')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
+    info.extend([info[-1] for _ in range(500 - len(info))])
+    plt.plot(axis_x_500, info, linewidth=1)
 
 plt.ylabel(y_label)
 plt.xlabel(x_label)
@@ -277,16 +269,12 @@ plt.savefig("../Plotting/Sequence Recall/NEAT_5_Standard.png", bbox_inches='tigh
 
 # 6-deep Report
 plt.figure(20)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../NEAT/Sequence Recall/6-deep-report/6-progress_report' + str(i) + '.csv')
-    info = []
-    with open(path, newline='\n') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            fitness = row[0].split()
-            info.append(float(fitness[0]))
-            axis_x = list(range(0, len(info)))
-    plt.plot(axis_x, info, linewidth=1)
+path = os.path.join(local_dir, '../NEAT/Sequence Recall/reports/6_report_std')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
+    info.extend([info[-1] for _ in range(500 - len(info))])
+    plt.plot(axis_x_500, info, linewidth=1)
 
 plt.ylabel(y_label)
 plt.xlabel(x_label)
@@ -296,16 +284,12 @@ plt.savefig("../Plotting/Sequence Recall/NEAT_6_Standard.png", bbox_inches='tigh
 
 # 15-deep Report
 plt.figure(21)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../NEAT/Sequence Recall/15-deep-report/15-progress_report' + str(i) + '.csv')
-    info = []
-    with open(path, newline='\n') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            fitness = row[0].split()
-            info.append(float(fitness[0]))
-            axis_x = list(range(0, len(info)))
-    plt.plot(axis_x, info, linewidth=1)
+path = os.path.join(local_dir, '../NEAT/Sequence Recall/reports/15_report_std')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
+    info.extend([info[-1] for _ in range(500 - len(info))])
+    plt.plot(axis_x_500, info, linewidth=1)
 
 plt.ylabel(y_label)
 plt.xlabel(x_label)
@@ -315,16 +299,12 @@ plt.savefig("../Plotting/Sequence Recall/NEAT_15_Standard.png", bbox_inches='tig
 
 # 21-deep Report
 plt.figure(22)
-for i in range(1,21):
-    path = os.path.join(local_dir, '../NEAT/Sequence Recall/21-deep-report/21-progress_report' + str(i) + '.csv')
-    info = []
-    with open(path, newline='\n') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            fitness = row[0].split()
-            info.append(float(fitness[0]))
-            axis_x = list(range(0, len(info)))
-    plt.plot(axis_x, info, linewidth=1)
+path = os.path.join(local_dir, '../NEAT/Sequence Recall/reports/21_report_std')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
+    info.extend([info[-1] for _ in range(500 - len(info))])
+    plt.plot(axis_x_500, info, linewidth=1)
 
 plt.ylabel(y_label)
 plt.xlabel(x_label)
