a026f53d795343a8db81f98ea7ad0e6407ba1851
Mihyar Al Masalma <eng.mihyear@gmail.com>
2022-08-24 19:50:31 -0300

    Additional Plotting

--- Plotting/gecco_paper.py
+++ Plotting/gecco_paper.py
@@ -57,6 +57,20 @@ for info in data.values():
     info.extend([info[-1] for _ in range(500 - len(info))])
     NEAT_5_Standard.append(info)
 
+# 6-deep Report DEAP
+path = os.path.join(local_dir, '../DEAP/Sequence Recall/reports/6_report_std')
+with open(path, 'rb') as f:
+    DEAP_6_Standard = list(pickle.load(f).values())
+
+# 6-deep Report NEAT
+NEAT_6_Standard = []
+path = os.path.join(local_dir, '../NEAT/Sequence Recall/reports/6_report_std')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
+    info.extend([info[-1] for _ in range(500 - len(info))])
+    NEAT_6_Standard.append(info)
+
 '''
     Sequence Classification
 '''
@@ -73,6 +87,20 @@ with open(path, 'rb') as f:
 for info in data.values():
     info.extend([info[-1] for _ in range(500 - len(info))])
     NEAT_5_Standard_2.append(info)
+
+# 6-deep Report DEAP
+path = os.path.join(local_dir, '../DEAP/Sequence Classification/reports/6_report_std')
+with open(path, 'rb') as f:
+    DEAP_6_Standard_2 = list(pickle.load(f).values())
+
+# 6-deep Report NEAT
+NEAT_6_Standard_2 = []
+path = os.path.join(local_dir, '../NEAT/Sequence Classification/reports/6_report_std')
+with open(path, 'rb') as f:
+    data = pickle.load(f)
+for info in data.values():
+    info.extend([info[-1] for _ in range(500 - len(info))])
+    NEAT_6_Standard_2.append(info)
 
 # 15-deep Report DEAP
 path = os.path.join(local_dir, '../DEAP/Sequence Classification/reports/15_report_std')
@@ -123,6 +151,16 @@ NEAT_5_Standard_std = np.std(NEAT_5_Standard, axis=0)
 NEAT_5_Standard_upperlimit = np.clip(np.add(NEAT_5_Standard_mean, NEAT_5_Standard_std), a_min=0, a_max=100)
 NEAT_5_Standard_lowerlimit = np.clip(np.subtract(NEAT_5_Standard_mean, NEAT_5_Standard_std), a_min=0, a_max=100)
 
+DEAP_6_Standard_mean = np.mean(DEAP_6_Standard, axis=0)
+DEAP_6_Standard_std = np.std(DEAP_6_Standard, axis=0)
+DEAP_6_Standard_upperlimit = np.clip(np.add(DEAP_6_Standard_mean, DEAP_6_Standard_std), a_min=0, a_max=100)
+DEAP_6_Standard_lowerlimit = np.clip(np.subtract(DEAP_6_Standard_mean, DEAP_6_Standard_std), a_min=0, a_max=100)
+
+NEAT_6_Standard_mean = np.mean(NEAT_6_Standard, axis=0)
+NEAT_6_Standard_std = np.std(NEAT_6_Standard, axis=0)
+NEAT_6_Standard_upperlimit = np.clip(np.add(NEAT_6_Standard_mean, NEAT_6_Standard_std), a_min=0, a_max=100)
+NEAT_6_Standard_lowerlimit = np.clip(np.subtract(NEAT_6_Standard_mean, NEAT_6_Standard_std), a_min=0, a_max=100)
+
 DEAP_5_Standard_2_mean = np.mean(DEAP_5_Standard_2, axis=0)
 DEAP_5_Standard_2_std = np.std(DEAP_5_Standard_2, axis=0)
 DEAP_5_Standard_2_upperlimit = np.clip(np.add(DEAP_5_Standard_2_mean, DEAP_5_Standard_2_std), a_min=0, a_max=100)
@@ -132,6 +170,16 @@ NEAT_5_Standard_2_mean = np.mean(NEAT_5_Standard_2, axis=0)
 NEAT_5_Standard_2_std = np.std(NEAT_5_Standard_2, axis=0)
 NEAT_5_Standard_2_upperlimit = np.clip(np.add(NEAT_5_Standard_2_mean, NEAT_5_Standard_2_std), a_min=0, a_max=100)
 NEAT_5_Standard_2_lowerlimit = np.clip(np.subtract(NEAT_5_Standard_2_mean, NEAT_5_Standard_2_std), a_min=0, a_max=100)
+
+DEAP_6_Standard_2_mean = np.mean(DEAP_6_Standard_2, axis=0)
+DEAP_6_Standard_2_std = np.std(DEAP_6_Standard_2, axis=0)
+DEAP_6_Standard_2_upperlimit = np.clip(np.add(DEAP_6_Standard_2_mean, DEAP_6_Standard_2_std), a_min=0, a_max=100)
+DEAP_6_Standard_2_lowerlimit = np.clip(np.subtract(DEAP_6_Standard_2_mean, DEAP_6_Standard_2_std), a_min=0, a_max=100)
+
+NEAT_6_Standard_2_mean = np.mean(NEAT_6_Standard_2, axis=0)
+NEAT_6_Standard_2_std = np.std(NEAT_6_Standard_2, axis=0)
+NEAT_6_Standard_2_upperlimit = np.clip(np.add(NEAT_6_Standard_2_mean, NEAT_6_Standard_2_std), a_min=0, a_max=100)
+NEAT_6_Standard_2_lowerlimit = np.clip(np.subtract(NEAT_6_Standard_2_mean, NEAT_6_Standard_2_std), a_min=0, a_max=100)
 
 DEAP_15_Standard_mean = np.mean(DEAP_15_Standard, axis=0)
 DEAP_15_Standard_std = np.std(DEAP_15_Standard, axis=0)
@@ -225,5 +273,26 @@ plt.xlabel(x_label)
 plt.legend(loc=legend_loc)
 plt.savefig("../Plotting/Gecco Paper/copy_task_modified.png", bbox_inches='tight')
 
+plt.figure(7)
+plt.plot(axis_x_250, DEAP_6_Standard_mean, linewidth=1, label="6-depth GP", color=color1)
+plt.fill_between(axis_x_250, DEAP_6_Standard_lowerlimit, DEAP_6_Standard_upperlimit, color=color1, alpha=.3)
+plt.plot(axis_x_500, NEAT_6_Standard_mean, linewidth=1, label="6-depth NEAT", color=color2)
+plt.fill_between(axis_x_500, NEAT_6_Standard_lowerlimit, NEAT_6_Standard_upperlimit, color=color2, alpha=.3)
+
+plt.ylabel(y_label)
+plt.xlabel(x_label)
 plt.legend(loc=legend_loc)
+plt.savefig("../Plotting/Gecco Paper/seq_recall_6.png", bbox_inches='tight')
+
+plt.figure(8)
+plt.plot(axis_x_250, DEAP_6_Standard_2_mean, linewidth=1, label="6-depth GP", color=color1)
+plt.fill_between(axis_x_250, DEAP_6_Standard_2_lowerlimit, DEAP_6_Standard_2_upperlimit, color=color1, alpha=.3)
+plt.plot(axis_x_500, NEAT_6_Standard_2_mean, linewidth=1, label="6-depth NEAT", color=color2)
+plt.fill_between(axis_x_500, NEAT_6_Standard_2_lowerlimit, NEAT_6_Standard_2_upperlimit, color=color2, alpha=.3)
+
+plt.ylabel(y_label)
+plt.xlabel(x_label)
+plt.legend(loc=legend_loc)
+plt.savefig("../Plotting/Gecco Paper/seq_class_6.png", bbox_inches='tight')
+
 plt.show()
